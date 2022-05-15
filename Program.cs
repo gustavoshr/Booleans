f5f2e9f11d96397045efcc65a243368ff33b975f
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:38:47 -0300

    booleans

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool estudar = true;
+            //bool procrastinar = false;
+            
+            int x = 10;
+            int y = 9;
+           
+            Console.WriteLine(x > y);
+
+            //console.writeLine(10 == 15);
+            
+        }
+    }
+}
